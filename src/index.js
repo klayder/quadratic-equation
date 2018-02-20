e31c4c1165cb9292244020bc787e687748c3a568
Klayder <anderysorokin@gmail.com>
2018-02-20 12:41:26 +0300

    my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+	var tempArr=equation.split(" ").join("").split("*");
+	var a=tempArr[0];
+	var b=tempArr[1].substring(3);
+	var c=tempArr[2].substring(1);
+	var discriminant=b*b-4*a*c;
+	var ansver=[Math.round((-b+Math.sqrt(discriminant))/(2*a)),Math.round((-b-Math.sqrt(discriminant))/(2*a))];
+ 	return ansver.sort((left, right) => left - right);
 }
